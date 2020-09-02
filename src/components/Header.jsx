26457a662ab8f15c9298fcb9df6a5a6563bb10d5
Cal <calvin.m71@hotmail.com>
2020-09-01 22:27:30 -0400

    Navbar and part of Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, Avatar, Grid, Box } from "material-ui/core";
+import me from "../me.png";
+
+const Header = () => {
+  return;
+  <Box>
+    <Avatar src={me} alt="Cal" />
+  </Box>;
+};
+
+export default Header;
