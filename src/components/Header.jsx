f78f49f9f1cfb56e183830f09e272b7ad4d87d02
Cal <calvin.m71@hotmail.com>
2020-09-02 00:12:08 -0400

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
-import { Typography, Avatar, Grid, Box } from "material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
 import Typed from "react-typed";
-import me from "../me.png";
+import me from "../image/me.png";
 
 //CSS
 const useStyles = makeStyles((theme) => ({
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import MobileRightMenuSlider from "@material-ui/core/Drawer";
+import ToolBar from "@material-ui/core/ToolBar";
 import {
   AppBar,
   ListItem,
@@ -8,7 +10,6 @@ import {
   IconButton,
   ListItemText,
   Avatar,
-  ToolBar,
   Divider,
   List,
   Typography,
@@ -21,7 +22,7 @@ import {
   Apps,
   ContactMail,
 } from "@material-ui/icons";
-import me from "../me.png";
+import me from "../image/me.png";
 
 //CSS
 const useStyles = makeStyles((theme) => ({
@@ -45,18 +46,22 @@ const menuIcons = [
   {
     listIcon: <Home />,
     listText: "Home",
+    listPath: "/",
   },
   {
     listIcon: <AssignmentInd />,
     listText: "Resume",
+    listPath: "/resume",
   },
   {
-    listIcon: <App />,
+    listIcon: <Apps />,
     listText: "Portfolio",
+    listPath: "/",
   },
   {
     listIcon: <ContactMail />,
     listText: "Contacts",
+    listPath: "/",
   },
 ];
 
@@ -80,8 +85,8 @@ const Navbar = () => {
       <Avatar className={classes.avatar} src={me} alt="Cal" />
       <Divider />
       <List>
-        {menuItems.map((lsItem, key) => (
-          <ListItem button key={key}>
+        {menuIcons.map((lsItem, key) => (
+          <ListItem button key={key} component={Link} to={lsItem.listPath}>
             <ListItemIcon className={classes.listItem}>
               {lsItem.listIcon}
             </ListItemIcon>
